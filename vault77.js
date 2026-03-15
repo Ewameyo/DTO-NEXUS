1e138da0376af62184f452e9568c8585ef429faa
Ewameyo <wameyoedgar417@gmail.com>
2026-03-15 22:53:47 +0300

    Create vault77.js

---
+++ vault77.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSy...", // Copy the long key from your Firebase settings
+    authDomain: "dto-nexus.firebaseapp.com",
+    databaseURL: "https://dto-nexus-default-rtdb.firebaseio.com",
+    projectId: "dto-nexus",
+    storageBucket: "dto-nexus.appspot.com",
+    messagingSenderId: "...", // Found in Firebase Project Settings
+    appId: "..." // Found in Firebase Project Settings
+};
